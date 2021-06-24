8d802423463c883485db3b92502734d18930a76b
Princh Canal <princhcanal@gmail.com>
2021-06-24 10:41:15 +0800

    move css and js files from activity 1 to root

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let links = document.querySelectorAll('.nav-links li');
+
+links.forEach((link) => {
+  link.addEventListener('click', () => {
+    links.forEach((link) => {
+      link.classList.remove('active');
+    });
+    link.classList.add('active');
+  });
+});
